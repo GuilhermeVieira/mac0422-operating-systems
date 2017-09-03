06dfed01312b21a4bba14a197639642a33348402
Victor <victorcga96@gmail.com>
2017-09-03 19:45:38 -0300

    things

--- fileHandlerr.c
+++ fileHandlerr.c
@@ -3,14 +3,13 @@
 List readFile(char *fileName)
 {
     List processQueue = createList();
-    int *n;
+    int n;
     FILE *file_pointer;
     char *line = NULL;
     char **parsedLine;
     size_t len = 0;
     ssize_t read;
     Process *temp;
-
     file_pointer = fopen(fileName, "r");
 
     if (file_pointer == NULL) {
@@ -18,15 +17,14 @@ List readFile(char *fileName)
         exit(EXIT_FAILURE);
     }
     while ((read = getline(&line, &len, file_pointer)) != -1) {
-        parsedLine = parseCommand(line, n);
-        if (*n != 4) {
-            fprintf(stderr, "ERRO: Formato das entradas inválido.");
-            exit(EXIT_FAILURE);
-        }
+        parsedLine = parseCommand(line, &n);
+        if(parsedLine[0][0] == '\0')
+            continue;
         temp = createProcess(atof(parsedLine[0]), atof(parsedLine[1]), atof(parsedLine[2]), parsedLine[3]);
         processQueue = addList(processQueue, temp);
     }
-    fclose (file_pointer); //
+    fclose (file_pointer);
+    return processQueue;
 }
 
 List createList()
--- scheduler.c
+++ scheduler.c
@@ -6,12 +6,12 @@ int main(int argc, char **argv)
 {
     int scheduler_type = atoi(argv[1]), optional = 0;
     char *inputFile, *outputFile;
+    List bob;
     inputFile = argv[2];
     outputFile = argv[3];
     if (argc >= 4 && strcmp("d", argv[4])){
         optional = 1;
     }
-    /*ver se o arquivo existe*/
-
+    bob = readFile(inputFile);
     return 0;
 }
