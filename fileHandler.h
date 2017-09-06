b75aee9aa19d697e82882e4efd706f9f2f64291c
Victor <victorcga96@gmail.com>
2017-09-06 16:02:17 -0300

    adds comments on the code

--- fileHandler.h
+++ fileHandler.h
@@ -10,7 +10,7 @@
 #include "buffer.h"
 
 typedef struct {
-                int line; // Linha do arquivo trace
+                int line;
                 double t0, dt, deadline, run_time;
                 char *name;
                 pthread_mutex_t mutex;
@@ -22,27 +22,51 @@ typedef struct node {
                     struct node *next;
                     } Cell;
 
-// Apontador para o ínicio da lista de processos.
 typedef Cell *List;
 
+/*Função de comparação usada pelo qsort para determinar qual processo tem dt menor.
+ */
 int cmpfunc(const void *a, const void *b);
 
+/*Função escreve as informações nescessaria sobre um processe (proc) em um
+ *arquivo de saida (outputFile) e recebe um número que se 1 ira imprimir
+ *informações adicionais na sterr.
+ */
 void writeFile(char *outputFile, Process *proc, double time, int optional);
 
+/*Função recebe o nome de um arquivo com processos e ira passar essas informações
+ *para uma lista ligada.
+ */
 List readFile(char *fileName);
 
+/*Cria a cabeça de uma lista.
+ */
 List createList();
 
+/*Adiciona um processe ao final de uma lista ligada não ordenada de processos.
+ */
 List addList(List root, Process *new_process);
 
+/*Remove um processo (indentificado pelo seu nome) da lista ligada fornecida;
+ */
 List removeList(List root, Process *x);
 
+/*Libera a memória usada por um processo.
+ */
 void destroyProcess(Process *x);
 
+/*Cria um processo que ira ser iniciado em t0, ira rodar por dt segundos, possui
+ *deadline igual a dl, um indetificador igual a name e se encontrava na linha
+ *line do arquivo de trace.
+ */
 Process *createProcess(double t0, double dt, double dl, char *name, int line);
 
-List add(List bob, List *bob2, double time, int optional);//
+/*
+ */
+List add(List bob, List *bob2, double time, int optional);
 
+/*Função retorna um ponteriro para a "tail" da lista ligada apontada por head.
+ */
 List getTail(List head);
 
 
--- scheduler.c
+++ scheduler.c
@@ -1,21 +1,23 @@
 // Guilherme Costa Vieira               Nº USP: 9790930
 // Victor Chiaradia Gramuglia Araujo    Nº USP: 9793756
 
-#include <string.h> //strcmp()
-#include <stdlib.h> //atoi()
-#include <time.h>  //nanosleep()
-#include <unistd.h> //sleep()
-#include <pthread.h>
-#include <math.h> //pow()
+#include <string.h> //strcmp().
+#include <stdlib.h> //atoi().
+#include <time.h>  //nanosleep(), clock().
+#include <unistd.h> //sleep().
+#include <pthread.h> //pthreads.
+#include <math.h> //pow().
 #include "fileHandler.h"
 
 #define QUANTUM 0.5
 #define NAP_TIME 0.5
 
-pthread_mutex_t mutex;
-double clock_time = 0;
-int cs_counter = 1;
+pthread_mutex_t mutex; //semáforo usado para parar os escalonadores.
+double clock_time = 0; //tempo que o programa usou para rodar.
+int cs_counter = 0; //número de mudanças de contexo.
 
+/*Função recebe um double e ira consumir tempo igual a esse número em segundos.
+ */
 void nap(double dt)
 {
     double temp;
@@ -23,16 +25,21 @@ void nap(double dt)
     temp = dt - temp2;
     temp *= 10;
     nanosleep((const struct timespec[]){{temp2, temp*100000000L}}, NULL);
-    clock_time += dt;
+    clock_time += dt; //atualização do tempo decorrido após o uso da função
     return;
 }
 
+/*Função recebe um ponteriro para void que ira ser transformado no tipo List e
+ *ira simular o processo apontado por ela.
+ */
 void *simulateProcSJF(void *toSchedule)
 {
     nap(((List)toSchedule)->info->dt);
     return NULL;
 }
 
+/*Função recebe uma parte de uma lista ligada e ira devolver o node com o menor dt.
+ */
 List getNextShortest(List root)
 {
     List shortest = root;
@@ -42,43 +49,53 @@ List getNextShortest(List root)
     }
     return shortest;
 }
-
+/*Função recebe o nome de dois arquivos, um de input e outro de output, junto
+ *com um int que se for 1 ira imprimir informações sobre os processos. A função
+ *simula um escalonador de processos do tipo Shortest Job First e imprime a
+ *saida dos processos no arquivo de output.
+ */
 void SJF(char *inputFile, char *outputFile, int optional)
 {
     pthread_t thread;
     List toArrive, shortest, toSchedule = NULL;
     toArrive = readFile(inputFile);
+    /*O escalonador deve funcionar até todos os processos tiverem sido escalonados
+     *e não há mais nehum processo para chegar.
+     */
     while((toSchedule != NULL || toArrive != NULL)){
-        clock_t begin = clock();
-        toSchedule = add(toSchedule, &toArrive, clock_time, optional);
-        printf("%f\n", clock_time);
+        clock_t begin = clock(); //calculo do tempo gasto pelo escalonador.
+        toSchedule = add(toSchedule, &toArrive, clock_time, optional); //ve se processos novos chegaram ao sistema.
         if (toSchedule == NULL){
-            nap(1);
+            nap(NAP_TIME);
             continue;
         }
-        shortest = getNextShortest(toSchedule);
+        shortest = getNextShortest(toSchedule); //procura qual processo devera ser escalonado.
         if (optional == 1)
             fprintf(stderr, "O processo %s começou usar a CPU %d\n", shortest->info->name, 1);
-        clock_t end = clock();
+        clock_t end = clock(); //calculo do tempo gasto pelo escalonador.
         double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
         clock_time += time_spent;
-        pthread_create(&thread, NULL, &simulateProcSJF, (void *)shortest);
-        pthread_join(thread, NULL);
-        begin = clock();
+        pthread_create(&thread, NULL, &simulateProcSJF, (void *)shortest); //simula o processo.
+        pthread_join(thread, NULL); //funciona como um semáforo para a função que chamou uma thread.
+        begin = clock(); //calculo do tempo gasto pelo escalonador.
         if (optional == 1)
             fprintf(stderr, "O processo %s deixou de usar a CPU %d\n", shortest->info->name, 1);
-        cs_counter++;
+        cs_counter++; //adiciona no número de mudançãs de contexto.
         if (optional == 1)
             fprintf(stderr, "Mudança de contexto %d\n", cs_counter);
         writeFile(outputFile, shortest->info, clock_time, optional);
-        toSchedule = removeList(toSchedule, shortest->info);
-        end = clock();
+        toSchedule = removeList(toSchedule, shortest->info); //como o processo acabou ele é removido da lista.
+        end = clock(); //calculo do tempo gasto pelo escalonador.
         time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
         clock_time += time_spent;
     }
     return;
 }
 
+/*Função recebe um processo e ira simular ela por um QUANTUM ou pelo tempo
+ *restante que ela possui se esse for menor que um QUANTUM, a função usa
+ *semáforos para simular a mudança de contexto.
+ */
 void *simulateProcRR(void *proc){
     Process *procc = (Process *)proc;
     while (procc->dt != procc->run_time){
@@ -90,61 +107,78 @@ void *simulateProcRR(void *proc){
         }
         else{
             nap(procc->dt - procc->run_time);
-            procc->run_time += procc->dt - procc->run_time; //mesma coisa de fazer proc->run_time = proc->dt;
+            procc->run_time += procc->dt - procc->run_time;
         }
         pthread_mutex_unlock(&mutex);
     }
     return NULL;
 }
 
+/*Função calcula a prioridade (número entre 1 e 10) de um dado processo, uma
+ *equação de segundo grau é usada devido sua simetria, quanto mais perto o tempo
+ *atual (clock_time) da deadline do processo mais prioridade o processo ganha,
+ *se a deadline de um processo passar depois de um certo tempo a prioriade vira
+ *uma constante.
+ */
 int calcPriority(Process *proc)
 {
+    //a função é -((clock_time - deadline)/dt)^2 + 10.
     double priority = -pow(((clock_time - proc->deadline)/proc->dt), 2) + 10;
     if (priority > 1) {
     	if ((int)(priority + 1) < 5 && clock_time > proc->deadline) return 5;
     	return (int)(priority + 1);
     }
-    return 1;
+    return 1; //defini um limite inferior para a função de segundo grau.
 }
 
+/*Função recebe um processo e ira simular ela por um múltiplo de um QUANTUM ou
+ *pelo tempo restante que ela possui se esse for menor que esse tempo, a função usa
+ *semáforos para simular a mudança de contexto.
+ */
 void *simulateProcPRR(void *proc){
 	int priority;
     Process *procc = (Process *)proc;
     while (procc->dt != procc->run_time){
         pthread_mutex_lock(&(procc->mutex));
-        printf("estou rodando o processo %s\n", procc->name);
         priority = calcPriority(procc);
-        printf("PRIORIDADE %d\n", priority);
         if (procc->dt - procc->run_time >= QUANTUM*priority){
             nap(QUANTUM*priority);
             procc->run_time += QUANTUM*priority;
         }
         else{
             nap(procc->dt - procc->run_time);
-            procc->run_time += procc->dt - procc->run_time; //mesma coisa de fazer proc->run_time = proc->dt;
+            procc->run_time += procc->dt - procc->run_time;
         }
         pthread_mutex_unlock(&mutex);
     }
     return NULL;
 }
 
+/*Função recebe o nome de dois arquivos, um de input e outro de output, junto
+ *com um int que se for 1 ira imprimir informações sobre os processos e um int
+ *que se 2 indica roundRobin se outro número indica Escalonador com prioridade.
+ *A função simula um escalonador de processos do tipo Round Robin ou Escalonador
+ *com prioridade e imprime a saida dos processos no arquivo de output.
+ */
 void roundRobin(char *inputFile, char *outputFile, int optional, int scheduler_type)
 {
     List toArrive, toSchedule = NULL;
     List currProcess = NULL;
     toArrive = readFile(inputFile);
     pthread_mutex_init(&mutex, NULL);
+    /*O escalonador deve funcionar até todos os processos tiverem sido escalonados
+     *e não há mais nehum processo para chegar.
+     */
     while(toSchedule != NULL || toArrive != NULL){
-        clock_t begin = clock();
-        toSchedule = add(toSchedule, &toArrive, clock_time, optional);
+        clock_t begin = clock(); //calculo do tempo gasto pelo escalonador.
+        toSchedule = add(toSchedule, &toArrive, clock_time, optional); //ve se processos novos chegaram ao sistema.
         if (toSchedule == NULL){
             nap(NAP_TIME);
             continue;
         }
-        if (currProcess == NULL) // Faz a lista ser circular
+        if (currProcess == NULL)
            currProcess = toSchedule;
-        //ver se ela já rodou, se não iniciar farol;
-        clock_t end = clock();
+        clock_t end = clock(); //calculo do tempo gasto pelo escalonador.
         double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
         clock_time += time_spent;
         if (currProcess->info->run_time == 0){
@@ -162,12 +196,15 @@ void roundRobin(char *inputFile, char *outputFile, int optional, int scheduler_t
             pthread_mutex_unlock(&(currProcess->info->mutex));
         }
         pthread_mutex_lock(&mutex);
-        begin = clock();
+        begin = clock(); //calculo do tempo gasto pelo escalonador.
         cs_counter++;
         if (optional == 1){
             fprintf(stderr, "O processo %s deixou de usar a CPU %d\n", currProcess->info->name, 1);
             fprintf(stderr, "Mudança de contexto %d\n", cs_counter);
         }
+        /* Se o processo acabou ele deve ser inserido no outputFile, ser retirado
+         *da lista de processos e ter sua thread terminada.
+         */
         if (currProcess->info->run_time >= currProcess->info->dt){
             writeFile(outputFile, currProcess->info, clock_time, optional);
             pthread_mutex_destroy(&(currProcess->info->mutex));
@@ -178,7 +215,7 @@ void roundRobin(char *inputFile, char *outputFile, int optional, int scheduler_t
             continue;
         }
         currProcess = currProcess->next;
-        end = clock();
+        end = clock(); //calculo do tempo gasto pelo escalonador.
         time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
         clock_time += time_spent;
     }
@@ -198,7 +235,6 @@ int main(int argc, char **argv)
         SJF(inputFile, outputFile, optional);
     else
         roundRobin(inputFile, outputFile, optional, scheduler_type);
-
     fp = fopen(outputFile, "a");
     fprintf(fp, "%d\n", cs_counter);
     fclose(fp);
