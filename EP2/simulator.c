d503e6c2f0b1cb80a900b0014154ccff5c37568e
PaiZuZe <vitinho_cga@hotmail.com>
2017-10-09 14:20:07 -0300

    Changes to the calculation of the speed

--- EP2/simulator.c
+++ EP2/simulator.c
@@ -29,24 +29,12 @@ void *emalloc(size_t size)
         return pointer;
 }
 
-double velInRefreshTime (int velocity, int refresh)
+double velInRefreshTime (double velocity, int refresh)
 {
-	if (velocity == 30)
-      	if (refresh == 2)
-      		return 0.5;
-
-        else
-      		return 1.0/6;
-
-    else if (velocity == 60)
-      	if (refresh == 2)
-      		return 1.0;
-
-        else
-      		return 1.0/3;
-
+    if (refresh == 2)
+        return (velocity/3600)*60;
     else
-      	return 0.5;
+        return (velocity/3600)*20;
 }
 
 int getNewVelocity (int velocity)
@@ -99,24 +87,37 @@ uint **initializeTrack(uint d, uint n)
 
 int updatePosition(position *pos, int lenth)
 {
-  	int new_pos = pos->x - 1;
-
-    if (new_pos < 0)
-      	new_pos = lenth - 1;
-
-    if (pista[new_pos][pos->y] == 0)
-      	pos->x = new_pos;
-
-    else
-      	return 1;
-
-    return 0;
+  	int new_pos_x = pos->x - 1;
+    int new_pos_y = pos->y;
+    /*Ve se tem que usar a circularidade da pista*/
+    if (new_pos_x < 0)
+      	new_pos_x = lenth - 1;
+    /*Ve se pode ir imediatamente para frente*/
+    if (pista[new_pos_x][pos->y] == 0){
+      	pos->x = new_pos_x;
+        /*Ir para à esquerda sempre que der*/
+        if (pos->y > 0 && pista[new_pos_x][pos->y - 1] == 0)
+            pos->y = new_pos_y - 1;
+        return 0;
+    }
+    /*Ver se pode ir para ultrapassar pela direita*/
+    else {
+        for (; new_pos_y < LANES; new_pos_y++)
+            if (pista[pos->x][new_pos_y] == 0 && pista[new_pos_x][new_pos_y] == 0){
+                pos->x = new_pos_x;
+                pos->y = new_pos_y;
+                return 0;
+            }
+    }
+    /*não pode se mover para nenhum lugar então tentara se mover na prox iteração*/
+    return 1;
 }
 
 
-void updateTrack(position *pos)
+void updateTrack(position *pos, position *old_pos, uint tag)
 {
-
+    pista[old_pos->x][old_pos->y] = 0;
+    pista[pos->x][pos->y] = tag;
     return;
 }
 
@@ -144,8 +145,9 @@ void *ciclista(void *args)
     int velocity = 30;
     uint laps = 0;
   	position *pos = emalloc(sizeof(position));
-  	pos->x = (arg->tag - 1)/10;
-  	pos->y = (arg->tag - 1)%10;
+    position *old_pos = emalloc(sizeof(position));
+  	pos->x = old_pos->x = (arg->tag - 1)/10;
+  	pos->y = old_pos->y = (arg->tag - 1)%10;
 
     pthread_mutex_init(&(sem_vec[arg->tag -1]), NULL);
     pthread_mutex_lock(&(sem_vec[arg->tag -1]));
@@ -154,24 +156,26 @@ void *ciclista(void *args)
       	pthread_mutex_lock(&(sem_vec[arg->tag -1]));
 
         if (!blocked)
-  			updatePos += ((int) velInRefreshTime(velocity, refresh)*refresh)%refresh;
+  			updatePos = (updatePos + ((int) velInRefreshTime(velocity, refresh)*refresh))%refresh;
 
         else
           	updatePos = 0;
 
         if (updatePos == 0){
   			blocked = updatePosition(pos, arg->n);
-
-            pthread_mutex_lock(&track_mutex);
-            updateTrack(pos);
-          	pthread_mutex_unlock(&track_mutex);
-
             if (pos->x == arg->d - 1){
   				velocity = getNewVelocity(velocity);
   				laps++;
   			}
         }
-		pthread_barrier_wait(&barrier);
+        /*espera todas as threads calcularem sua nova pos*/
+        pthread_barrier_wait(&barrier);
+        /*só uma thread pode mudar a sua pos por vez, ainda pode bugar, o cara manda pro valor certo e dps um cara manda para um errado*/
+        pthread_mutex_lock(&track_mutex);
+        updateTrack(pos, old_pos, arg->tag);
+        pthread_mutex_unlock(&track_mutex);
+        /*espera todo mundo mudar sua pos*/
+        pthread_barrier_wait(&barrier);
   	}
   	return NULL;
 }
