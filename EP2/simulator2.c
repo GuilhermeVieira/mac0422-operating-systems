023a84c7a69cf5c7b21aeebe9bcba150154be89e
guilhermevieira <xgvieira@gmail.com>
2017-10-11 18:17:31 -0300

    Fix bug terminating thread dummies

--- EP2/simulator2.c
+++ EP2/simulator2.c
@@ -1,4 +1,4 @@
-#include <stdio.h> 	//fprintf().
+#include <stdio.h>  //fprintf().
 #include <stdlib.h> //atoi(), malloc(), rand(), srand().
 #include <time.h> //time().
 #include <pthread.h> //pthreads.
@@ -15,10 +15,10 @@ pthread_mutex_t n_cyclists_mutex;
 
 pthread_barrier_t barrier1;
 pthread_barrier_t barrier2;
-pthread_t *threadD;
+pthread_t *thread_dummy;
 uint n_cyclists = 0;
-int *mov;
 uint **pista;
+int is_over;
 
 void *emalloc(size_t size)
 {
@@ -35,20 +35,26 @@ void *emalloc(size_t size)
         return pointer;
 }
 
-void *printMatrix(void *arg)
+void *report(void *arg)
 {
     uint d = *((uint *) arg);
-  	while (n_cyclists > 0) {
-      	pthread_barrier_wait(&barrier1);
-	    for (int i = 0; i < d; i++){
-    	   for (int j = 0; j < LANES; j++)
-        	    printf("%d ", pista[i][j]);
-        	printf("\n");
-    	}
-    	printf("\n");
-      	pthread_barrier_wait(&barrier2);
+    while (1) {
+      pthread_barrier_wait(&barrier1);
+
+      if (n_cyclists > 0) {
+            for (int i = 0; i < d; i++) {
+               for (int j = 0; j < LANES; j++)
+                    printf("%d ", pista[i][j]);
+                printf("\n");
+            }
+            printf("\n");
+      }
+
+      if (is_over) break;
+      pthread_barrier_wait(&barrier2);
     }
-  	return NULL;
+
+    return NULL;
 }
 
 double velInRefreshTime (int velocity, int refresh)
@@ -66,84 +72,84 @@ double velInRefreshTime (int velocity, int refresh)
 
 int getNewVelocity (int velocity)
 {
-  	int prob;
+    int prob;
 
-  	prob = rand()%100;
+    prob = rand()%100;
 
     if (velocity == 30)
-      	if (prob <= 30)
-      		return velocity;
+        if (prob <= 30)
+            return velocity;
 
         else
-      		return 60;
+            return 60;
 
     else if (velocity == 60)
-      	if (prob <= 50)
-      		return 30;
+        if (prob <= 50)
+            return 30;
 
         else
-      		return velocity;
+            return velocity;
 
     else
-      	return velocity;
+        return velocity;
 }
 
 uint **initializeTrack(uint d, uint n)
 {
-	uint **new_track;
-  	uint tag = 1;
-  	new_track = emalloc(d*sizeof(uint *));
+    uint **new_track;
+    uint tag = 1;
+    new_track = emalloc(d*sizeof(uint *));
 
     for (int i = 0; i < d; i++){
-  		new_track[i] = emalloc(LANES*sizeof(uint));
+        new_track[i] = emalloc(LANES*sizeof(uint));
 
         for (int j = 0; j < LANES; j++){
 
             if (tag <= n){
-          		new_track[i][j] = tag;
-            	tag++;
+                new_track[i][j] = tag;
+                tag++;
             }
 
             else
-        		new_track[i][j] = 0;
-      	}
+                new_track[i][j] = 0;
+        }
     }
-  	return new_track;
+    return new_track;
 }
 
 int updatePosition(position *pos, int length)
 {
-  	int new_pos_x = pos->x - 1;
+    int new_pos_x = pos->x - 1;
     int new_pos_y = pos->y;
     /*Ve se tem que usar a circularidade da pista*/
     if (new_pos_x < 0)
-      	new_pos_x = length - 1;
+        new_pos_x = length - 1;
 
-  	/* Vai pra esquerda e pra frente (diagonal) ultimo if verifica se n é ultrapassagem pela interna */
+    /* Vai pra esquerda e pra frente (diagonal) ultimo if verifica se n é ultrapassagem pela interna */
     if (pos->y > 0 && pista[pos->x][pos->y - 1] == 0 && pista[new_pos_x][pos->y - 1] == 0 && pista[new_pos_x][pos->y] == 0){
-		pos->x = new_pos_x;
-      	pos->y = new_pos_y - 1;
-      	return 0;
+        pos->x = new_pos_x;
+        pos->y = new_pos_y - 1;
+        return 0;
     }
 
     /*Ve se pode ir imediatamente para frente*/
     if (pista[new_pos_x][pos->y] == 0){
-      	pos->x = new_pos_x;
+        pos->x = new_pos_x;
         return 0;
     }
     /*Ver se pode ir para ultrapassar pela direita*/
     else {
-      	for (; new_pos_y < LANES; new_pos_y++){
-          	if (pista[pos->x][new_pos_y] == 0)
-          		if (pista[new_pos_x][new_pos_y] == 0){
-        			pos->x = new_pos_x;
-               		pos->y = new_pos_y;
-                	return 0;
-        		}
-          		else
-                  	continue;
-          	else
-          		break;
+        for (; new_pos_y < LANES; new_pos_y++){
+            if (pista[pos->x][new_pos_y] == 0)
+                if (pista[new_pos_x][new_pos_y] == 0){
+                    pos->x = new_pos_x;
+                    pos->y = new_pos_y;
+                    return 0;
+                }
+                else
+                    continue;
+            else
+                break;
         }
     }
     /*não pode se mover para nenhum lugar então tentara se mover na prox iteração*/
@@ -159,18 +165,20 @@ void updateTrack(position *pos, position *old_pos, uint tag)
 
 void destroyTrack(uint **track, uint d)
 {
-  	for (int i = 0; i < d; i++)
-  		free(track[i]);
-	free(track);
-  	return;
+    for (int i = 0; i < d; i++)
+        free(track[i]);
+    free(track);
+    return;
 }
 
 void *dummy()
 {
     while (1){
         pthread_barrier_wait(&barrier1); //espera todo mundo atualizar sua pos
-      	pthread_barrier_wait(&barrier2);
+        if (is_over) break;
+        pthread_barrier_wait(&barrier2);
     }
+    printf("SAI\n");
     return NULL;
 }
 
@@ -179,80 +187,98 @@ void *ciclista(void *args)
     thread_arg *arg = (thread_arg *) args;
     int velocity = 30;
     int refresh = 2;
-  	int updatePos = 0;
+    int updatePos = 0;
     int blocked = 0; // Vale 1 se o ciclista frente está bloqueando a passagem.
     uint laps = 0;
     position *pos = emalloc(sizeof(position));
     position *old_pos = emalloc(sizeof(position));
-  	pos->x = (arg->tag - 1)/10;
-  	pos->y = (arg->tag - 1)%10;
+    pos->x = (arg->tag - 1)/10;
+    pos->y = (arg->tag - 1)%10;
+    
+    /* Loop que simula a corrida. */
     while (laps <= arg->v){
-		old_pos->x = pos->x;
-      	old_pos->y = pos->y;
+        old_pos->x = pos->x;
+        old_pos->y = pos->y;
 
-        if (!blocked){
-  			updatePos = (updatePos + ((int) (velInRefreshTime(velocity, refresh)*refresh)))%refresh;
+        /* Calcula a nova posição. */
+        if (!blocked) {
+            updatePos = (updatePos + ((int) (velInRefreshTime(velocity, refresh)*refresh)))%refresh;
         }
-        else{
-          	updatePos = 0;
+        else {
+            updatePos = 0;
         }
-        if (updatePos == 0){
-  			blocked = updatePosition(pos, arg->d);
+        if (updatePos == 0) {
+            blocked = updatePosition(pos, arg->d);
             if (pos->x == arg->d - 1){
-  				velocity = getNewVelocity(velocity);
-  				laps++;
-  			}
+                velocity = getNewVelocity(velocity);
+                laps++;
+            }
         }
-      	pthread_barrier_wait(&barrier1); //espera todo mundo atualizar sua pos
-        updateTrack(pos, old_pos, arg->tag);
-      	pthread_barrier_wait(&barrier2);
-    }
-    pista[pos->x][pos->y] = 0;
 
+        pthread_barrier_wait(&barrier1); // Espera todo mundo calcular sua posição.
+        updateTrack(pos, old_pos, arg->tag); // Atualiza a posição na pista.
+        pthread_barrier_wait(&barrier2); // Espera todo mundo atualizar sua posição na pista.
+    }
+
+    /* Tira o ciclista da pista. */
+    pista[pos->x][pos->y] = 0; // ACHO QUE PRECISA COLOCAR UMAS VERIFICAÇOES PARA NAO SOBRESCREVER
+
+    /* Atualiza o número de ciclistas na pista. */
     pthread_mutex_lock(&n_cyclists_mutex);
     n_cyclists--;
     pthread_mutex_unlock(&n_cyclists_mutex);
 
-    pthread_create(&(threadD[arg->tag - 1]), NULL, &dummy, NULL);
-    pthread_join(threadD[arg->tag - 1], NULL);
+    /* Verifica se a corrida acabou e manda um sinal para todos pararem. */
+    if (n_cyclists < 1) is_over = 1;
+
+    /* Cria uma thread dummy para deixar a barreira funcional. */
+    pthread_create(&(thread_dummy[arg->tag - 1]), NULL, &dummy, NULL);
+
+    /* Espera a thread dummy acabar, ou seja, a corrida acabou . */
+    pthread_join(thread_dummy[arg->tag - 1], NULL);
+    
     free(pos);
     free(old_pos);
+    
     return NULL;
 }
 
 int main(int argc, char **argv)
 {
-  	uint d = atoi(argv[1]);
-  	uint n = atoi(argv[2]);
-  	uint v = atoi(argv[3]);
+    /* Inicializando variáveis locais. */
+    uint d = atoi(argv[1]);
+    uint n = atoi(argv[2]);
+    uint v = atoi(argv[3]);
     pthread_t thread[n + 1];
 
-    threadD = emalloc(n*sizeof(pthread_t));
+    /* Inicializando variáveis globais. */
+    is_over = 0;
     n_cyclists = n;
-  	thread_arg *args = emalloc(n*sizeof(thread_arg));
-    mov = emalloc(n*sizeof(int));
-    for (int i = 0; i < n; i++){
-        mov[i] = 0;
-    }
-  	pista = initializeTrack(d, n);
-
+    thread_arg *args = emalloc(n*sizeof(thread_arg));
+    thread_dummy = emalloc(n*sizeof(pthread_t));
+    pista = initializeTrack(d, n);
     pthread_mutex_init(&n_cyclists_mutex, NULL);
-
-    pthread_barrier_init(&barrier1, NULL ,n);
-    pthread_barrier_init(&barrier2, NULL ,n);
+    pthread_barrier_init(&barrier1, NULL ,n+1); 
+    pthread_barrier_init(&barrier2, NULL ,n+1); 
     srand(time(NULL));
-  	for (uint i = 0; i < n; i++){
-      	args[i].d = d;
-      	args[i].v = v;
-      	args[i].tag = i + 1;
-      	pthread_create(&(thread[i]), NULL, &ciclista, &args[i]);
+    
+    /* Cria as threads ciclistas. */
+    for (uint i = 0; i < n; i++){
+        args[i].d = d;
+        args[i].v = v;
+        args[i].tag = i + 1;
+        pthread_create(&(thread[i]), NULL, &ciclista, &args[i]);
     }
-    pthread_create(&(thread[n + 1]), NULL, &printMatrix, &d);
+
+    /* Cria a thread que realiza o Relatório e o debug da corrida. */
+    pthread_create(&(thread[n + 1]), NULL, &report, &d);
+    
+    /* Espera todos os ciclistas terminarem a prova. */
     for (int i = 0; i < n; i++)
-        pthread_join(thread[i], NULL);
+        pthread_join(thread[i], NULL); // ACHO QUE TEM QUE ESPERAR A REPORT TAMBÉM!!!
 
     free(args);
     destroyTrack(pista, d);
 
-  	return 0;
+    return 0;
 }
