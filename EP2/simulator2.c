ac7dbf99f860b3ccb5fc59c9bd0f936de0080e3b
PaiZuZe <vitinho_cga@hotmail.com>
2017-10-10 21:31:07 -0300

    In with the new

--- EP2/simulator2.c
+++ EP2/simulator2.c
@@ -11,7 +11,10 @@ typedef struct { uint d, v, tag;} thread_arg;
 typedef struct { int x, y;} position;
 
 pthread_mutex_t track_mutex;
+pthread_barrier_t barrier1;
+pthread_barrier_t barrier2;
 uint n_cyclists = 0;
+int *mov;
 uint **pista;
 
 void *emalloc(size_t size)
@@ -153,6 +156,69 @@ void destroyTrack(uint **track, uint d)
   	return;
 }
 
+void canUpdate(position pos, uint d)
+{
+	int i = pos.x;
+  	for (int j = 0; j < LANES; j++)
+      	if (pista[i][j] != 0)
+      		while (mov[pista[i][j] - 1] != 1){}
+	return;
+}
+
+void canMov(position pos, uint d)
+{
+  	int i = pos.x - 1;
+  	if (i == -1)
+      	i = d - 1;
+  	for (int j = 0; j < LANES; j++)
+      	if (pista[i][j] != 0)
+      		while (mov[pista[i][j] - 1] != 2){}
+  	return;
+}
+
+void *ciclista(void *args)
+{
+    thread_arg *arg = (thread_arg *) args;
+    int velocity = 30;
+    int refresh = 2;
+  	int updatePos = 0;
+    int blocked = 0; // Vale 1 se o ciclista frente está bloqueando a passagem.
+    uint laps = 0;
+    position *pos = emalloc(sizeof(position));
+    position *old_pos = emalloc(sizeof(position));
+  	pos->x = (arg->tag - 1)/10;
+  	pos->y = (arg->tag - 1)%10;
+    while (laps <= arg->v){
+		old_pos->x = pos->x;
+      	old_pos->y = pos->y;
+    	canMov(*pos, arg->d);
+        if (!blocked){
+  			updatePos = (updatePos + ((int) (velInRefreshTime(velocity, refresh)*refresh)))%refresh;
+        }
+        else{
+          	updatePos = 0;
+        }
+        if (updatePos == 0){
+  			blocked = updatePosition(pos, arg->d);
+            if (pos->x == arg->d - 1){
+  				velocity = getNewVelocity(velocity);
+  				laps++;
+  			}
+        }
+      	mov[arg->tag -1] = 1;
+      	canUpdate(*pos, arg->d);
+        updateTrack(pos, old_pos, arg->tag);
+      	mov[arg->tag -1] = 2;
+
+      	pthread_barrier_wait(&barrier1); //espera todo mundo atualizar sua pos
+      	mov[arg->tag -1] = 0;
+      	pthread_barrier_wait(&barrier2);
+    }
+    free(pos);
+    free(old_pos);
+    return NULL;
+}
+
 int main(int argc, char **argv)
 {
   	uint d = atoi(argv[1]);
@@ -162,8 +228,13 @@ int main(int argc, char **argv)
 
     n_cyclists = n;
   	thread_arg *args = emalloc(n*sizeof(thread_arg));
+    mov = emalloc(n*sizeof(int));
+    for (int i = 0; i < n; i++){
+        mov[i] = 0;
+    }
   	pista = initializeTrack(d, n);
-	pthread_mutex_init(&track_mutex, NULL);
+    pthread_barrier_init(&barrier1, NULL ,n);
+    pthread_barrier_init(&barrier2, NULL ,n);
     srand(time(NULL));
   	for (uint i = 0; i < n; i++){
       	args[i].d = d;
@@ -174,6 +245,7 @@ int main(int argc, char **argv)
     for (int i = 0; i < n; i++)
         pthread_join(thread[i], NULL);
 
+    free(args);
     destroyTrack(pista, d);
 
   	return 0;
