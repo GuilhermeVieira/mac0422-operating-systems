87a1452fb4b43d37e4c52ab5271fb7dbcec50b68
Victor C Araujo <vitinho_cga@hotmail.com>
2017-11-03 18:18:28 -0200

    More things

--- EP3/ep3.py
+++ EP3/ep3.py
@@ -1,4 +1,5 @@
 import sys
+from time import sleep
 
 class process:
     def __init__(self, t0, tf, b, name) :
@@ -29,15 +30,21 @@ def analyseCommand(command, sim_parameters) :
         print("Comando invalido!")
     return run
 
+def arrive(to_arrive, l_procs, time) :
+    for i in to_arrive :
+        if (i.t0 == time) :
+            l_procs.append(i)
+    to_arrive[:] = [x for x in to_arrive if not x.t0 == time]
+
 def read_file(sim_parameters, to_arrive, compact) :
     file = open(sim_parameters[0], "r")
     line = file.readline()
-    tot, vit, s, p = line.split()[0], line.split()[1], line.split()[2], line.split()[3]
+    tot, vit, s, p = int(line.split()[0]), int(line.split()[1]), int(line.split()[2]), int(line.split()[3])
     for line in file :
         if (len(line.split()) == 2) :
             compact.append(line.split()[0])
             continue
-        to_arrive.append(process(line.split()[0],line.split()[1],line.split()[2],line.split()[3]))
+        to_arrive.append(process(int(line.split()[0]),int(line.split()[1]),int(line.split()[2]),line.split()[3]))
         for i in range(4, len(line.split()), 2) :
             to_arrive[-1].addExec(int(line.split()[i]), int(line.split()[i + 1]))
     file.close()
@@ -45,19 +52,36 @@ def read_file(sim_parameters, to_arrive, compact) :
 
 def simulation(sim_parameters) :
     to_arrive = []
-    l_procs = []
     compact = []
+    l_procs = []
+    v_mem = []
+    p_mem = []
     time = 0
     tot, vit, s, p = read_file(sim_parameters, to_arrive, compact)
     print(tot, vit, s, p)
     for i in to_arrive :
         i.printProc()
     #agora criar os arquivos de memória
-
+    for i in range (tot//p) :
+        p_mem.append(-1)
+    for i in range (vit//p) :
+        v_mem.append(-1)
+    print(p_mem)
+    print(v_mem)
     #começa o loop do processo
     while (to_arrive or l_procs) :
-        to_arrive.pop()
-        #faz a simulação
+        #colocar os caras da to_arrive na l_procs
+        arrive(to_arrive, l_procs, time)
+        for i in to_arrive :
+            i.printProc()
+        print("######")
+        for i in l_procs :
+            i.printProc()
+        print("//////")
+        #percorrer a l_procs para executar os algs
+        for i in l_procs :
+            if (i.tf == time) :
+                l_procs.remove(i)
         time += 1
     return
 
