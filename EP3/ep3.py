d59b39ff580bebc623f2df534d8364c0e6c341fb
Victor C Araujo <vitinho_cga@hotmail.com>
2017-11-03 09:58:12 -0200

    No more global variables

--- EP3/ep3.py
+++ EP3/ep3.py
@@ -1,19 +1,15 @@
 import sys
-#substituir por um objeto seria legal
-def analyseCommand(command):
+
+def analyseCommand(command, sim_parameters) :
     run = 0
     if (command.split()[0] == "carrega") :
-        global file_name
-        file_name = command.split()[1]
+        sim_parameters[0] = command.split()[1]
     elif (command.split()[0] == "espaco") :
-        global mem_alg
-        mem_alg = command.split()[1]
+        sim_parameters[1] = int(command.split()[1])
     elif (command.split()[0] == "substitui") :
-        global page_alg
-        page_alg = command.split()[1]
+        sim_parameters[2] = int(command.split()[1])
     elif (command.split()[0] == "executa") :
-        global del_Time
-        del_Time = command.split()[1]
+        sim_parameters[3] = int(command.split()[1])
         run = 1
     elif (command.split()[0] == "sair") :
         sys.exit()
@@ -21,14 +17,15 @@ def analyseCommand(command):
         print("Comando invalido!")
     return run
 
-def simulation() :
-    print("irei fazer coisas")
+def simulation(sim_parameters) :
+    print(sim_parameters)
 
 def main() :
+    sim_parameters = ["", 0, 0, 1]
     while(True) :
         print("[ep3] :", end = "")
         command = input()
-        if(analyseCommand(command)) :
-            simulation()
+        if(analyseCommand(command, sim_parameters)) :
+            simulation(sim_parameters)
 
 main()
